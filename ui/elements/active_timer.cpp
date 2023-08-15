830ad9d416417486286fb27c8db5d940f399e15e
Hector Ledesma <hectoremil21@hotmail.com>
2023-08-15 10:59:22 -0400

    Add toggle pause functionality to interface timer.

--- ui/elements/active_timer.cpp
+++ ui/elements/active_timer.cpp
@@ -4,7 +4,7 @@
 #include "cpp/imgui_stdlib.h"
 
 namespace UI {
-    Timer::Timer(ImGuiWindowFlags flags, data::Timer timer) : m_timer(timer) {
+    Timer::Timer(ImGuiWindowFlags flags, data::Timer &timer) : m_timer(timer) {
         setFlags(flags);
     }
 
@@ -23,17 +23,37 @@ namespace UI {
         // Date
         ImGui::Text(data::Timer::dateToString(m_timer.getDate()).c_str());
         // Timer runtime
-        std::chrono::system_clock::time_point elapsed{ std::chrono::system_clock::now() - m_timer.getStartTime() };
+        std::chrono::system_clock::time_point elapsed;
+        if (m_paused) {
+            elapsed = std::chrono::system_clock::time_point{ paused_at - m_timer.getStartTime() };
+        }
+        else {
+            elapsed = std::chrono::system_clock::time_point{ std::chrono::system_clock::now() - m_timer.getStartTime() };
+        }
         ImGui::Text(data::Timer::durationToString(elapsed).c_str());
 
         ImGui::SameLine();
         if (ImGui::Button("Play/Pause")) {
             // Play pause functionality
+            togglePause();
         }
         ImGui::SameLine();
         if (ImGui::Button("Stop")) {
             // Stop timer functionality
         }
+    }
 
+    void Timer::togglePause() {
+        if (m_paused) {
+            // unpause
+            std::chrono::system_clock::time_point prev_elapsed{paused_at - m_timer.getStartTime()};
+            std::chrono::system_clock::time_point new_time{ std::chrono::system_clock::now() - prev_elapsed};
+            m_timer.updateTime(new_time);
+        }
+        else {
+            // pause
+            paused_at = std::chrono::system_clock::now();
+        }
+        m_paused = !m_paused;
     }
 }
--- ui/elements/active_timer.h
+++ ui/elements/active_timer.h
@@ -9,13 +9,18 @@
 namespace UI {
     class Timer : public Element {
     public:
-        Timer(ImGuiWindowFlags flags, data::Timer timer);
+        Timer(ImGuiWindowFlags flags, data::Timer &timer);
     protected:
         bool begin();
         void end();
         void style();
     private:
         data::Timer m_timer;
+        std::chrono::system_clock::time_point paused_at;
+        bool m_paused = false;
+
+        void togglePause();
+
     };
 }
 #endif
