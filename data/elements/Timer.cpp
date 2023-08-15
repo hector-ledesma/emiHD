af4f380c9a6ef9c8933d112ef8fe7d9df76431ac
Hector Ledesma <hectoremil21@hotmail.com>
2023-08-15 06:56:33 -0400

    Define TIMER_RUNNING constant. Add isRunning() to Timer.

--- data/elements/Timer.cpp
+++ data/elements/Timer.cpp
@@ -3,7 +3,7 @@
 namespace data {
 
     Timer::Timer(int id, std::time_t date, std::string title, std::string comments,
-        high_resolution_clock::time_point startTime, high_resolution_clock::time_point endTime) :
+        system_clock::time_point startTime, system_clock::time_point endTime) :
         m_id(id), m_date(date), m_title(title), m_comments(comments), m_startTime(startTime), m_endTime(endTime)
     {
     }
@@ -20,14 +20,17 @@ namespace data {
     std::string Timer::getComments() {
         return m_comments;
     }
-    high_resolution_clock::time_point Timer::getStartTime() {
+    system_clock::time_point Timer::getStartTime() {
         return m_startTime;
     }
-    high_resolution_clock::time_point Timer::getEndTime() {
+    system_clock::time_point Timer::getEndTime() {
         return m_endTime;
     }
     float Timer::getDuration() {
-        std::chrono::duration<float, std::milli> elapsed = std::chrono::high_resolution_clock::now() - m_startTime;
+        std::chrono::duration<float, std::milli> elapsed = std::chrono::system_clock::now() - m_startTime;
         return elapsed.count();
+    }
+    bool Timer::isRunning() {
+        return m_endTime == TIMER_RUNNING;
     }
 }
--- data/elements/Timer.h
+++ data/elements/Timer.h
@@ -5,30 +5,34 @@
 #include <string>
 #include <chrono>
 #include <ctime>
-
+// MACROS can be defined for: Defining an end time value when timer is "active",
+//  checking if timer is active.
+using namespace std::chrono_literals;
+constexpr std::chrono::time_point<std::chrono::system_clock> TIMER_RUNNING{0s};
 
 namespace data {
-    using std::chrono::high_resolution_clock;
+    using std::chrono::system_clock;
     // TODO:- There should be an interface namespace Timer with limited constructor and methods that
     //      the backend controller translates for storage.
     class Timer {
     public:
         Timer(int id, std::time_t date, std::string title, std::string comments,
-            high_resolution_clock::time_point startTime, high_resolution_clock::time_point endTime);
+            system_clock::time_point startTime, system_clock::time_point endTime = TIMER_RUNNING);
 
         int getId();
         std::time_t getDate();
         std::string getTitle();
         std::string getComments();
-        high_resolution_clock::time_point getStartTime();
-        high_resolution_clock::time_point getEndTime();
+        system_clock::time_point getStartTime();
+        system_clock::time_point getEndTime();
         float getDuration();
+        bool isRunning();
     private:
         int m_id;
-        std::string m_title;
+        std::string m_title; // Title should eventually be replaced with "tasks"/"tags"
         std::time_t m_date;
-        high_resolution_clock::time_point m_startTime;
-        high_resolution_clock::time_point m_endTime;
+        system_clock::time_point m_startTime;
+        system_clock::time_point m_endTime;
         std::string m_comments; // Will be its own data structure down the line
     };
 }
